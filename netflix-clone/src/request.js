efe13fe695da1fc0d1aca8b91fff5ac341d0ee73
samueltiokeng <samueledward40@gmail.com>
2020-09-08 04:47:11 +0200

    a

--- netflix-clone/src/request.js
+++ netflix-clone/src/request.js
@@ -3,5 +3,5 @@ const API_KEY = "010cf02106be78cd97145ccefed2ec7d";
 const requests = {
     fetchTrending: '/trending/all/week!api_key=${API_KEY}&language=en-US',
     fetchNetflixOriginals: '/discover/tv?api_key=${API_KEY}&with_networks=213',
-    fetchTopRated:
+    fetchTopRated: '/movie/top-related?api_key=${API_KEY}%la'
 }
