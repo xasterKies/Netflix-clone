545936432e5814192ebe6aae20c004d6aa098932
samueltiokeng <samueledward40@gmail.com>
2020-09-08 20:52:18 +0200

    a

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending now"fetchUrl={requests.fetchTrending}/>
+      <Row title="Trending now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
