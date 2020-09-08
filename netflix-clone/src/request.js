ddc879954dfd4edf236209f23730259411fc5ba9
samueltiokeng <samueledward40@gmail.com>
2020-09-08 06:43:44 +0200

    ad

--- netflix-clone/src/request.js
+++ netflix-clone/src/request.js
@@ -6,7 +6,8 @@ const requests = {
     fetchTopRated: '/movie/top-related?api_key=${API_KEY}&language=en-US',
     fetchActionMovies: '/discover/movie?api_key=${API_KEY}&with_genres=28',
     fetchComedyMovies: '/discover/movie?api_key=${API_KEY}&with_genres=35',
-    fetchHorrorMovies: '/discover/movie?api_key${API_KEY}&with_genres=27'
-    
+    fetchHorrorMovies: '/discover/movie?api_key=${API_KEY}&with_genres=27'
+    fetchRomanceMovies: '/discover/movie?api_key=${API_KEY}&with_genres=10749'
+
 
 }
