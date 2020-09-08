89b2d2877785eb1260d239531a9bb2e81ffd7376
samueltiokeng <samueledward40@gmail.com>
2020-09-08 22:50:29 +0200

    a

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -6,6 +6,7 @@ const base_url = "htttps://image.tmdb.org/t/p/original/";
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
 
+
     useEffect(() => {
         async function fetchData(){
             const request = await axios.get(fetchUrl);
@@ -23,8 +24,9 @@ function Row({title, fetchUrl}) {
             <div className="row__posters">
                 {/*several row__posters */}
                 {movies.map(movie => (
-                <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/> 
-                ))}
+                <img src={`${base_url}+${movie.poster_path}`} alt={movie.name}/> 
+                ))
+                }
                 
             </div>
 
