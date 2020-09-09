3c1a7dae43cd99e65f3623e1875aff121689783e
samueltiokeng <samueledward40@gmail.com>
2020-09-09 17:08:16 +0200

    b

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -21,7 +21,7 @@ function Banner() {
     }, []);
 
     function truncate(str, n){
-        return str?.length > n ? str.substr(0, n-1) + "..." : str:
+        return str?.length > n ? str.substr(0, n-1) + "..." : str;
     }
 
 
@@ -45,7 +45,7 @@ function Banner() {
                 </div>
 
                 <h1 className="banner__description">
-                    {truncate(movie?.overview}}
+                    {truncate(movie?.overview, 150)}
                 </h1>
             </div>
         </header>
