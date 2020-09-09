5012942177dd5e839292c25dbfa19ae8ed3fd1a0
samueltiokeng <samueledward40@gmail.com>
2020-09-09 06:59:08 +0200

    b

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,13 +1,14 @@
 import React, { useState,useEffect } from 'react'
+import { from "a";
 
 function Banner() {
     const [movie, setMovie] = useState([]);
 
     useEffect(() => {
         async function fetchData(){
-            
+
         }
-        
+        fetchData();
     }, [])
 
 
