dfd5c4d5ebc23228edb61153ce537339fd3181ce
samueltiokeng <samueledward40@gmail.com>
2020-09-09 16:27:25 +0200

    b

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import axios from "./axios";
 import requests from "./requests";
-import from "./Banner.css"
+import "./Banner.css"
 
 function Banner() {
     const [movie, setMovie] = useState([]);
