3afa6dea392d6119c7e0afd98f4204ee44149417
samueltiokeng <samueledward40@gmail.com>
2020-09-09 04:48:41 +0200

    r

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import "./Row.css"
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -27,7 +28,7 @@ function Row({title, fetchUrl}) {
                 <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/> 
                 ))
                 }
-                
+            
             </div>
 
             {/* container -> poster */}
