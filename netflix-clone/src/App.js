6b06f096d76122e15a26c4d0d313a536efb76385
samueltiokeng <samueledward40@gmail.com>
2020-09-09 06:45:16 +0200

    banner

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from '/Banner';
 
 function App() {
 
   return (
     <div className="app">
-      <h1>Netflix clone</h1>
+      <Banner/>
       <Row title="NETFLIX ORIGINALS"
        fetchUrl={requests.fetchNetflixOriginals}
        isLargeRow
