f9c75374227a8e8ecc7df6924ec93820319a4801
samueltiokeng <samueledward40@gmail.com>
2020-09-09 05:23:22 +0200

    a

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -6,7 +6,10 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="NETFLIX Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending}/>
+      <Row title="A now" fetchUrl={requests.fetchTrending}/>
       <Row title="Trending now" fetchUrl={requests.fetchTrending}/>
       <Row title="Trending now" fetchUrl={requests.fetchTrending}/>
       <Row title="Trending now" fetchUrl={requests.fetchTrending}/>
