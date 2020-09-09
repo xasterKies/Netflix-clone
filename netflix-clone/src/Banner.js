470f2c6bb13b682f39f726173811c735bd9a03b2
samueltiokeng <samueledward40@gmail.com>
2020-09-09 06:50:23 +0200

    b

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Banner() {
-    const [movie, setMovie] = useState
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        
+        
+    }, [input])
 
 
     return (
