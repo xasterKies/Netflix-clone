52ae46e488312218a2e16f3038b6f29b18eb0fb1
samueltiokeng <samueledward40@gmail.com>
2020-09-09 16:01:38 +0200

    b

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -25,7 +25,8 @@ function Banner() {
         <header className="banner"
           style={{
               backgroundSize: "cover",
-              backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`
+              backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+              backgroundPosition: "center"
           }}
         >
             <div className="banner__contents">
