81fa0fdea20e6d4f507dfcadfdc4444d4dfb7e34
samueltiokeng <samueledward40@gmail.com>
2020-09-09 16:38:13 +0200

    b

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -31,7 +31,7 @@ function Banner() {
           }}
         >
             <div className="banner__contents">
-                <h1>
+                <h1 className="banner__title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner__buttons">
