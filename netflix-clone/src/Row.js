aa4722aebfecce73702991bd392055bdc7ed833c
samueltiokeng <samueledward40@gmail.com>
2020-09-10 05:55:22 +0200

    t

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import "./Row.css";
 import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
