5a900e773efb02b5fbc14baf0e15a79a8b3fcdf4
samueltiokeng <samueledward40@gmail.com>
2020-09-10 04:37:01 +0200

    n

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -2,22 +2,22 @@ import React from 'react'
 import './Nav.css'
 
 function Nav() {
+    const [show, handleShow]
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100){
+                handleShow(true);
+            } else handleShow(false);
+        });
+
+        return () => {
+            window.removeEventListener("scroll");
+        }
+        
+    }, []);
     return (
         <div className="nav">
-
-            useEffect(() => {
-                window.addEventListener("scroll", () => {
-                    if (window.scrollY > 100){
-                        handleShow(true);
-                    } else handleShow(false);
-                });
-
-                return () => {
-
-                    window.removeEventListener("scroll");
-                }
-                
-            }, []);
             <img
             className="nav__logo"
             src="https://pmcvariety.files.wordpress.com/2020/05/netflix-logo.png?w=600"
