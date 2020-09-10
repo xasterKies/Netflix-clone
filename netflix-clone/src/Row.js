a9c2bcf658c3d7508dec5b02b8a4b403035c6927
samueltiokeng <samueledward40@gmail.com>
2020-09-10 05:42:29 +0200

    t

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -39,7 +39,7 @@ function Row({title, fetchUrl, isLargeRow}) {
                 <img
                  key={movie.id}
                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                 onClick={() => hand}
+                 onClick={() => handleClick(movie)}
 
                  src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/> 
                 ))
